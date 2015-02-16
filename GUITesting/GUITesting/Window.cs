8ce81cecbb4701ae362fc0d28874a9b56ed84d5b
hendrikwiersma <hendrikwiersma@gmail.com>
2015-02-16 03:12:28 +0100

    Some drawing done.

--- GUITesting/GUITesting/Window.cs
+++ GUITesting/GUITesting/Window.cs
@@ -7,6 +7,8 @@ namespace GUITesting
 	public partial class Window : Gtk.Window
 	{
 		public Context cr;
+		bool drawing = false;
+		PointD beginpoint;
 		string mode = "Rectangle";
 		public Window () : base (Gtk.WindowType.Toplevel)
 		{
@@ -39,16 +41,25 @@ namespace GUITesting
 		{
 			
 			poslabel.Text = args.Event.X + "x" + args.Event.Y;
+			if (drawing == true) {
+				drawingarea1.QueueDraw();
+				cr.Save ();
+				cr.SetSourceRGB (0.0, 0.0, 0.0);
+				double width = args.Event.X - beginpoint.X;
+				double height = args.Event.Y - beginpoint.Y;
+				cr.LineWidth = 1;
+				cr.Rectangle(beginpoint.X, beginpoint.Y, width, height);
+				cr.StrokePreserve();
+				cr.Restore ();
+			}
 		}
 
 		protected void OnDrawingarea1ButtonPressEvent (object o, ButtonPressEventArgs args)
 		{
-			cr.SetSourceRGB(colorbutton1.Color.Red, colorbutton1.Color.Green, colorbutton1.Color.Blue);
-			cr.Save ();
-			cr.Scale (0.5, 1.0);
-			cr.Arc(args.Event.X, args.Event.Y, 50, 0, 2*Math.PI);
-			cr.Fill();
-			cr.Restore();
+			drawing = true;
+			beginpoint.X = args.Event.X;
+			beginpoint.Y = args.Event.Y;
+
 		}
 		protected void OnDawingarea1PointerMotion(){
 
@@ -62,6 +73,38 @@ namespace GUITesting
 		protected void OnDrawingarea1ScrollEvent (object o, ScrollEventArgs args)
 		{
 			Console.WriteLine ("Yup it scrolls");
+		}
+
+		protected void OnDrawingarea1ButtonReleaseEvent (object o, ButtonReleaseEventArgs args)
+		{
+			drawing = false;
+			Console.WriteLine ("Released at "+args.Event.X + "x" + args.Event.Y);
+			cr.Save ();
+
+			if (mode == "Rectangle") {
+				cr.SetSourceRGB (colorbutton1.Color.Red, colorbutton1.Color.Green, colorbutton1.Color.Blue);
+				double width = args.Event.X - beginpoint.X;
+				double height = args.Event.Y - beginpoint.Y;
+				cr.LineWidth = 1;
+				cr.Rectangle(beginpoint.X, beginpoint.Y, width, height);
+				cr.StrokePreserve();
+
+				cr.Fill();
+
+			} else if (mode == "Ellipse") {
+				cr.SetSourceRGB(colorbutton1.Color.Red, colorbutton1.Color.Green, colorbutton1.Color.Blue);
+				cr.Scale (0.5, 1.0);
+				cr.Arc(args.Event.X, args.Event.Y, 50, 0, 2*Math.PI);
+				cr.Fill();
+			}
+			//cr.PushGroup ();
+			cr.Restore();
+		}
+
+		protected void OnButton1Clicked (object sender, EventArgs e)
+		{
+
+			//Pattern tn = cr.PopGroup ();
 		}
 	}
 }
--- GUITesting/GUITesting/gtk-gui/GUITesting.Window.cs
+++ GUITesting/GUITesting/gtk-gui/GUITesting.Window.cs
@@ -10,6 +10,8 @@ namespace GUITesting
 		
 		private global::Gtk.Table table3;
 		
+		private global::Gtk.Button button1;
+		
 		private global::Gtk.ColorButton colorbutton1;
 		
 		private global::Gtk.ComboBox combobox1;
@@ -19,6 +21,8 @@ namespace GUITesting
 		private global::Gtk.Label label2;
 		
 		private global::Gtk.Label label3;
+		
+		private global::Gtk.Label label4;
 		
 		private global::Gtk.Label poslabel;
 		
@@ -46,88 +50,112 @@ namespace GUITesting
 			w1.RightAttach = ((uint)(3));
 			w1.XOptions = ((global::Gtk.AttachOptions)(7));
 			// Container child table1.Gtk.Table+TableChild
-			this.table3 = new global::Gtk.Table (((uint)(3)), ((uint)(2)), false);
+			this.table3 = new global::Gtk.Table (((uint)(4)), ((uint)(2)), false);
 			this.table3.Name = "table3";
 			this.table3.RowSpacing = ((uint)(6));
 			this.table3.ColumnSpacing = ((uint)(6));
+			// Container child table3.Gtk.Table+TableChild
+			this.button1 = new global::Gtk.Button ();
+			this.button1.CanFocus = true;
+			this.button1.Name = "button1";
+			this.button1.UseUnderline = true;
+			this.button1.Label = global::Mono.Unix.Catalog.GetString ("Undo");
+			this.table3.Add (this.button1);
+			global::Gtk.Table.TableChild w2 = ((global::Gtk.Table.TableChild)(this.table3 [this.button1]));
+			w2.TopAttach = ((uint)(3));
+			w2.BottomAttach = ((uint)(4));
+			w2.LeftAttach = ((uint)(1));
+			w2.RightAttach = ((uint)(2));
+			w2.XOptions = ((global::Gtk.AttachOptions)(4));
+			w2.YOptions = ((global::Gtk.AttachOptions)(4));
 			// Container child table3.Gtk.Table+TableChild
 			this.colorbutton1 = new global::Gtk.ColorButton ();
 			this.colorbutton1.CanFocus = true;
 			this.colorbutton1.Events = ((global::Gdk.EventMask)(784));
 			this.colorbutton1.Name = "colorbutton1";
 			this.table3.Add (this.colorbutton1);
-			global::Gtk.Table.TableChild w2 = ((global::Gtk.Table.TableChild)(this.table3 [this.colorbutton1]));
-			w2.LeftAttach = ((uint)(1));
-			w2.RightAttach = ((uint)(2));
-			w2.XOptions = ((global::Gtk.AttachOptions)(4));
-			w2.YOptions = ((global::Gtk.AttachOptions)(4));
-			// Container child table3.Gtk.Table+TableChild
-			this.combobox1 = global::Gtk.ComboBox.NewText ();
-			this.combobox1.AppendText (global::Mono.Unix.Catalog.GetString ("Rectangle"));
-			this.combobox1.AppendText (global::Mono.Unix.Catalog.GetString ("Elipse"));
-			this.combobox1.Name = "combobox1";
-			this.combobox1.Active = 0;
-			this.table3.Add (this.combobox1);
-			global::Gtk.Table.TableChild w3 = ((global::Gtk.Table.TableChild)(this.table3 [this.combobox1]));
-			w3.TopAttach = ((uint)(1));
-			w3.BottomAttach = ((uint)(2));
+			global::Gtk.Table.TableChild w3 = ((global::Gtk.Table.TableChild)(this.table3 [this.colorbutton1]));
 			w3.LeftAttach = ((uint)(1));
 			w3.RightAttach = ((uint)(2));
 			w3.XOptions = ((global::Gtk.AttachOptions)(4));
 			w3.YOptions = ((global::Gtk.AttachOptions)(4));
 			// Container child table3.Gtk.Table+TableChild
+			this.combobox1 = global::Gtk.ComboBox.NewText ();
+			this.combobox1.AppendText (global::Mono.Unix.Catalog.GetString ("Rectangle"));
+			this.combobox1.AppendText (global::Mono.Unix.Catalog.GetString ("Ellipse"));
+			this.combobox1.Name = "combobox1";
+			this.combobox1.Active = 0;
+			this.table3.Add (this.combobox1);
+			global::Gtk.Table.TableChild w4 = ((global::Gtk.Table.TableChild)(this.table3 [this.combobox1]));
+			w4.TopAttach = ((uint)(1));
+			w4.BottomAttach = ((uint)(2));
+			w4.LeftAttach = ((uint)(1));
+			w4.RightAttach = ((uint)(2));
+			w4.XOptions = ((global::Gtk.AttachOptions)(4));
+			w4.YOptions = ((global::Gtk.AttachOptions)(4));
+			// Container child table3.Gtk.Table+TableChild
 			this.label1 = new global::Gtk.Label ();
 			this.label1.Name = "label1";
 			this.label1.LabelProp = global::Mono.Unix.Catalog.GetString ("Select Color:");
 			this.table3.Add (this.label1);
-			global::Gtk.Table.TableChild w4 = ((global::Gtk.Table.TableChild)(this.table3 [this.label1]));
-			w4.XOptions = ((global::Gtk.AttachOptions)(0));
-			w4.YOptions = ((global::Gtk.AttachOptions)(4));
+			global::Gtk.Table.TableChild w5 = ((global::Gtk.Table.TableChild)(this.table3 [this.label1]));
+			w5.XOptions = ((global::Gtk.AttachOptions)(0));
+			w5.YOptions = ((global::Gtk.AttachOptions)(4));
 			// Container child table3.Gtk.Table+TableChild
 			this.label2 = new global::Gtk.Label ();
 			this.label2.Name = "label2";
 			this.label2.LabelProp = global::Mono.Unix.Catalog.GetString ("Select Shape:");
 			this.table3.Add (this.label2);
-			global::Gtk.Table.TableChild w5 = ((global::Gtk.Table.TableChild)(this.table3 [this.label2]));
-			w5.TopAttach = ((uint)(1));
-			w5.BottomAttach = ((uint)(2));
-			w5.XOptions = ((global::Gtk.AttachOptions)(4));
-			w5.YOptions = ((global::Gtk.AttachOptions)(4));
+			global::Gtk.Table.TableChild w6 = ((global::Gtk.Table.TableChild)(this.table3 [this.label2]));
+			w6.TopAttach = ((uint)(1));
+			w6.BottomAttach = ((uint)(2));
+			w6.XOptions = ((global::Gtk.AttachOptions)(4));
+			w6.YOptions = ((global::Gtk.AttachOptions)(4));
 			// Container child table3.Gtk.Table+TableChild
 			this.label3 = new global::Gtk.Label ();
 			this.label3.Name = "label3";
 			this.label3.LabelProp = global::Mono.Unix.Catalog.GetString ("Position:");
 			this.table3.Add (this.label3);
-			global::Gtk.Table.TableChild w6 = ((global::Gtk.Table.TableChild)(this.table3 [this.label3]));
-			w6.TopAttach = ((uint)(2));
-			w6.BottomAttach = ((uint)(3));
-			w6.XOptions = ((global::Gtk.AttachOptions)(4));
-			w6.YOptions = ((global::Gtk.AttachOptions)(4));
+			global::Gtk.Table.TableChild w7 = ((global::Gtk.Table.TableChild)(this.table3 [this.label3]));
+			w7.TopAttach = ((uint)(2));
+			w7.BottomAttach = ((uint)(3));
+			w7.XOptions = ((global::Gtk.AttachOptions)(4));
+			w7.YOptions = ((global::Gtk.AttachOptions)(4));
+			// Container child table3.Gtk.Table+TableChild
+			this.label4 = new global::Gtk.Label ();
+			this.label4.Name = "label4";
+			this.label4.LabelProp = global::Mono.Unix.Catalog.GetString ("Undo:");
+			this.table3.Add (this.label4);
+			global::Gtk.Table.TableChild w8 = ((global::Gtk.Table.TableChild)(this.table3 [this.label4]));
+			w8.TopAttach = ((uint)(3));
+			w8.BottomAttach = ((uint)(4));
+			w8.XOptions = ((global::Gtk.AttachOptions)(4));
+			w8.YOptions = ((global::Gtk.AttachOptions)(4));
 			// Container child table3.Gtk.Table+TableChild
 			this.poslabel = new global::Gtk.Label ();
 			this.poslabel.Name = "poslabel";
 			this.poslabel.LabelProp = global::Mono.Unix.Catalog.GetString ("X:Y");
 			this.table3.Add (this.poslabel);
-			global::Gtk.Table.TableChild w7 = ((global::Gtk.Table.TableChild)(this.table3 [this.poslabel]));
-			w7.TopAttach = ((uint)(2));
-			w7.BottomAttach = ((uint)(3));
-			w7.LeftAttach = ((uint)(1));
-			w7.RightAttach = ((uint)(2));
-			w7.XOptions = ((global::Gtk.AttachOptions)(4));
-			w7.YOptions = ((global::Gtk.AttachOptions)(4));
-			this.table1.Add (this.table3);
-			global::Gtk.Table.TableChild w8 = ((global::Gtk.Table.TableChild)(this.table1 [this.table3]));
-			w8.XOptions = ((global::Gtk.AttachOptions)(0));
-			w8.YOptions = ((global::Gtk.AttachOptions)(4));
-			// Container child table1.Gtk.Table+TableChild
-			this.vseparator1 = new global::Gtk.VSeparator ();
-			this.vseparator1.Name = "vseparator1";
-			this.table1.Add (this.vseparator1);
-			global::Gtk.Table.TableChild w9 = ((global::Gtk.Table.TableChild)(this.table1 [this.vseparator1]));
+			global::Gtk.Table.TableChild w9 = ((global::Gtk.Table.TableChild)(this.table3 [this.poslabel]));
+			w9.TopAttach = ((uint)(2));
+			w9.BottomAttach = ((uint)(3));
 			w9.LeftAttach = ((uint)(1));
 			w9.RightAttach = ((uint)(2));
 			w9.XOptions = ((global::Gtk.AttachOptions)(4));
 			w9.YOptions = ((global::Gtk.AttachOptions)(4));
+			this.table1.Add (this.table3);
+			global::Gtk.Table.TableChild w10 = ((global::Gtk.Table.TableChild)(this.table1 [this.table3]));
+			w10.XOptions = ((global::Gtk.AttachOptions)(0));
+			w10.YOptions = ((global::Gtk.AttachOptions)(4));
+			// Container child table1.Gtk.Table+TableChild
+			this.vseparator1 = new global::Gtk.VSeparator ();
+			this.vseparator1.Name = "vseparator1";
+			this.table1.Add (this.vseparator1);
+			global::Gtk.Table.TableChild w11 = ((global::Gtk.Table.TableChild)(this.table1 [this.vseparator1]));
+			w11.LeftAttach = ((uint)(1));
+			w11.RightAttach = ((uint)(2));
+			w11.XOptions = ((global::Gtk.AttachOptions)(4));
+			w11.YOptions = ((global::Gtk.AttachOptions)(4));
 			this.Add (this.table1);
 			if ((this.Child != null)) {
 				this.Child.ShowAll ();
@@ -136,9 +164,11 @@ namespace GUITesting
 			this.DefaultHeight = 528;
 			this.Show ();
 			this.combobox1.Changed += new global::System.EventHandler (this.OnCombobox1Changed);
+			this.button1.Clicked += new global::System.EventHandler (this.OnButton1Clicked);
 			this.drawingarea1.ButtonPressEvent += new global::Gtk.ButtonPressEventHandler (this.OnDrawingarea1ButtonPressEvent);
 			this.drawingarea1.ExposeEvent += new global::Gtk.ExposeEventHandler (this.OnDrawingarea1ExposeEvent);
 			this.drawingarea1.MotionNotifyEvent += new global::Gtk.MotionNotifyEventHandler (this.OnDrawingarea1MotionNotifyEvent);
+			this.drawingarea1.ButtonReleaseEvent += new global::Gtk.ButtonReleaseEventHandler (this.OnDrawingarea1ButtonReleaseEvent);
 		}
 	}
 }
